8c21214bfe41f556c4fb5be32a9a2875a24496a7
Kalu Kalu <1718221904@qq.com>
2017-10-26 17:15:53 +0800

    minor change

--- pull_words.py
+++ pull_words.py
@@ -2,7 +2,6 @@ sample_vocab_list_url = "https://www.vocabulary.com/lists/181261"
 author_page = "https://www.vocabulary.com/profiles/A1F53BBSTZZTSF"
 url = author_page
 
-
 from urllib.request import urlopen
 
 print("\n\n attempting to open ")
