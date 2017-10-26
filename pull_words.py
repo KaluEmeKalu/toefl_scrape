46bb1ae2a205b12733ed9d3db796068269f1db94
Kalu Kalu <1718221904@qq.com>
2017-10-26 18:12:39 +0800

    got contents of first vocab list and save text file

--- pull_words.py
+++ pull_words.py
@@ -25,9 +25,6 @@ def save_text_file(text, filename):
         f.write(text)
 
 
-# Now We Want the First 2
-# Vocab Links From Len_W's 
-# Vocabulary.com profile Page
 def get_links(html_text):
 
     text = html_text
@@ -36,53 +33,26 @@ def get_links(html_text):
 
     next_word_position = text.find("wordlist shortlisting")
 
-    start_link_position = text.find("/lists/", next_word_position)
-    start_link_position += 7
-    end_link_position = text.find('"', start_link_position)
-    link = text[start_link_position: end_link_position]
-    link = link_start + link
-    link_list.append(link)
+    while next_word_position != -1:
+        start_link_position = text.find("/lists/", next_word_position)
+        start_link_position += 7
+        end_link_position = text.find('"', start_link_position)
+        link = text[start_link_position: end_link_position]
+        link = link_start + link
+        link_list.append(link)
+        next_word_position = text.find("wordlist shortlisting", end_link_position)
 
-    ################################
-    ################################
-    #### Now Let's Get The 2nd Link
-    ################################
-    ################################
-
-    # We simply get the next word position
-    next_word_position = text.find("wordlist shortlisting", end_link_position)
-
-    # Then We Copy and paste lines 39 to line 44
-    start_link_position = text.find("/lists/", next_word_position)
-    start_link_position += 7
-    end_link_position = text.find('"', start_link_position)
-    link = text[start_link_position: end_link_position]
-    link = link_start + link
-    link_list.append(link)
-
-    ################################
-    ################################
-    #### Now Let's Get The 3rd Link
-    ################################
-    ################################
-    
-    # Just Copy And Paste Fro Line 53
-    next_word_position = text.find("wordlist shortlisting", end_link_position)
-
-    # And Again We Copy and paste lines 39 to line 44
-    start_link_position = text.find("/lists/", next_word_position)
-    start_link_position += 7
-    end_link_position = text.find('"', start_link_position)
-    link = text[start_link_position: end_link_position]
-    link = link_start + link
-    link_list.append(link)
 
     return link_list
 
 
-our_filename = "vocab_link_list.txt"
-with open(our_filename, 'r') as file:
+with open("vocab_link_list.txt", 'r') as file:
     our_html_text = file.read()
 
 our_links = get_links(our_html_text)
-print(our_links)
+first_link = our_links[0]
+
+# Let's Open One HTML File & Save The Contents
+html_text = get_html_text(first_link)
+with open("first_vocab_link.txt", 'w') as file:
+    file.write(html_text)
