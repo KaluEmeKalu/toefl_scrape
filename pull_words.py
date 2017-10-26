000f743199767b54e06114b782ef39e3c872a989
Kalu Kalu <1718221904@qq.com>
2017-10-26 17:28:47 +0800

    added function

--- pull_words.py
+++ pull_words.py
@@ -1,15 +1,22 @@
 from urllib.request import urlopen
 
 url = "https://www.vocabulary.com/profiles/A1F53BBSTZZTSF"
-print("\n\n attempting to open ")
 
-# Get HTML Response From Website
-response = urlopen(url)
-# Read HTML Response Data
-data = response.read()  # a `bytes` object
-# Convert Bytes Data into UTF-8 String
-html_text = data.decode('utf-8')  # a `str`object;
+# We Turn Our Code Into A get_html_text
+# Function So That Our Code is Resuable
+def get_html_text(url):
 
+    print("\n\n attempting to open ")
+
+    # Get HTML Response From Website
+    response = urlopen(url)
+    # Read HTML Response Data
+    data = response.read()  # a `bytes` object
+    # Convert Bytes Data into UTF-8 String
+    html_text = data.decode('utf-8')  # a `str`object;
+
+    print("\n\n finished")
+    return html_text
+
+html_text = get_html_text(url)
 print(html_text)
-print("\n\n finished")
-
